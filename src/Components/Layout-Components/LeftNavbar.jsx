62807d75dc1ed1912a8de39b2a8ba9237923b956
Rupon <fardiidlamrupon@gmail.com>
2024-11-13 21:56:35 +0600

    category section completed

--- src/Components/Layout-Components/LeftNavbar.jsx
+++ src/Components/Layout-Components/LeftNavbar.jsx
@@ -1,9 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
  
 
 const LeftNavbar = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        axios('https://openapi.programming-hero.com/api/news/categories')
+        .then(result => {
+            setCategories(result.data.data.news_category)
+        })
+    } ,[])
+
     return (
-        <div>
+        <div className="space-y-2">
             <h2 className="font-semibold">All Category</h2>
+            <div className="flex flex-col gap-2">
+                {
+                    categories.map(category => 
+                      <button
+                      className="p-2 border rounded text-gray-500 hover:bg-base-300 hover:text-gray-800 transition-all"
+                      key={category.category_id}>{category.category_name}</button>)
+                }
+            </div>
         </div>
     );
 };
